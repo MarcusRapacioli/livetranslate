ca850adb25a322ec76474051ce2d25bf15bb2c72
Wesley Sprague <wesleysprague@yahoo.com>
2018-08-28 15:37:45 +0100

    added methods to lessons controller, views for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,5 @@
 class LessonsController < ApplicationController
+  before_action :set_lesson, only: [:show,:edit, :update, :destroy]
   def index
     @lessons = Lesson.all
   end
@@ -11,20 +12,39 @@ class LessonsController < ApplicationController
     @lesson = Lesson.new
   end
 
-  def create
-    @lesson = Lesson.new(lesson_params)
-    @lesson.teacher = current_user
-    if lesson.save
-      render root_path
+  def edit
+  end
+
+  def update
+    if @lesson.update(lesson_params)
+      redirect_to lesson_path(@lesson)
     else
       render :new
     end
   end
 
+  def create
+    @lesson = Lesson.new(lesson_params)
+    @lesson.teacher = current_user
+    if @lesson.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @lesson.destroy
+    redirect_to root_path
+  end
 
   private
   def lesson_params
     params.require(:lesson).permit(:lesson_name, :description, :teacher)
+  end
+
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
   end
 
 end
