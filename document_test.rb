ae33c31da338cd1dd198bb1682c1dda36228dc64
yuichiro toyama <ytoyama1872@gmail.com>
2018-08-29 16:58:52 +0100

    sentence count assigner method complete

--- document_test.rb
+++ document_test.rb
@@ -2,13 +2,55 @@ require 'pdf/reader'
 
 reader = PDF::Reader.new("lorem.pdf")
 
-paragraph = ""
-paragraphs = []
+
+sentence_array = []
 reader.pages.each do |page|
   clean = page.text.delete("\n")
-  lines = clean.scan(/[^\.\!\?]*[\.\!\?]/)
-  # p lines
-  p lines[3]
+  sentence_array << clean.scan(/[^\.\!\?]*[\.\!\?]/)
+end
+
+sentences = sentence_array.flatten
+
+students = %w[donald ivanka theresa jeremy]
+
+number_of_sentences = sentences.count
+p number_of_sentences
+
+x = number_of_sentences / students.count
+
+h = Hash[students.map {|student| [student, x]}]
+
+
+remainder = number_of_sentences % students.count
+
+
+# lucky_student = students.sample
+if remainder != 0
+  students_dup = h.dup
+  unlucky_students = h.dup
+  (students.count - remainder).times do
+    random_student = students_dup.keys.sample
+    unlucky_students.delete(random_student)
+    students_dup.delete(random_student)
+  end
+
+  unlucky_students.each do |student, _count|
+    h[student] += 1
+  end
+end
+
+p h.values # => [7, 7, 7, 6]
+
+first_para = sentences.first(h.values[0])
+first_sentence_index = sentences.index(first_para.last)
+second_para = sentences[((first_sentence_index + 1)..(first_sentence_index + (h.values[1])))]
+second_sentence_index = sentences.index(second_para.last)
+
+p first_para
+p second_para
+p second_sentence_index
+
+students.count.times do
 
 end
 
