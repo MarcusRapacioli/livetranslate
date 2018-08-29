dd4b99c9029c4f9a76d17a83c39b7d14b9ca5d54
Georges-Alexandre Haines <georgeshaines@gmail.com>
2018-08-29 17:58:34 +0100

    Profile page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,17 @@
 import "bootstrap";
 
-import '../components/fullpage';
+// import '../components/fullpage';
+// fullPage();
+// fullpage_api.setAllowScrolling(false);
 
-fullPage();
-fullpage_api.setAllowScrolling(false);
+
+// Profile tabs
+import { openCity } from '../components/tabs';
+
+const tabs = document.querySelectorAll('.tablinks')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', event => {
+    openCity(event, event.target.dataset["city"])
+  })
+})
