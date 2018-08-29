df277cc51f9757d34aeb0e5a0c78f066bb419e54
yuichiro toyama <ytoyama1872@gmail.com>
2018-08-29 14:06:54 +0100

    some minor pdf changes

--- document_test.rb
+++ document_test.rb
@@ -1,65 +1,14 @@
-require 'pdf-reader'
+require 'pdf/reader'
 
-reader = PDF::Reader.new("test one page.pdf")
-
-# puts reader.page_count
-
-
-# reader.pages.each do |page|
-#   puts page.text
-# end
-
-
-# reader.pages.each do |page|
-#   page.delete(/\n/, '')
-#   p page.text
-# end
-
-# # p paragraph
-
+reader = PDF::Reader.new("lorem.pdf")
 
 paragraph = ""
 paragraphs = []
 reader.pages.each do |page|
-  lines = page.text.scan(/\n\n/)
+  clean = page.text.delete("\n")
+  lines = clean.scan(/[^\.\!\?]*[\.\!\?]/)
   # p lines
-  # lines.each do |line|
-  #   if line.length > 55
-  #     paragraph += " #{line}"
-  #   else
-  #     paragraph += " #{line}"
-  #     paragraphs << paragraph
-  #     paragraph = ""
-  #   end
+  p lines[3]
+
 end
 
-# p paragraphs
-
-# text = File.read "test.txt"
-# p text.scan(/[^\.!?]+[\.!?]/).map(&:strip)
-
-# puts "#{text.split("\n").size} lines."
-# paragraphs = text.split ".\n"
-# puts "#{paragraphs.length} paragraphs."
-
-
-# p paragraphs
-
-ARGV.each do |lorem|
-  PDF::Reader.open(lorem) do |reader|
-    puts "Converting : #{lorem}"
-    pageno = 0
-    txt = reader.pages.map do |page|
-      pageno += 1
-      begin
-        print "Converting Page #{pageno}/#{reader.page_count}\r"
-        page.text
-      rescue
-        puts "Page #{pageno}/#{reader.page_count} Failed to convert"
-        ''
-      end
-    end # pages map
-    puts "\nWriting text to disk"
-    File.write lorem+'.txt', txt.join("\n")
-  end # reader
-end # each
