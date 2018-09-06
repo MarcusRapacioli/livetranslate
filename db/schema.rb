c6693aeb2a2e26a0364ce774a2f87915ba24d98d
Marcus Rapacioli <marcus.rapacioli@gmail.com>
2018-09-06 19:37:28 +0100

    checked

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_101721) do
+ActiveRecord::Schema.define(version: 2018_09_06_183238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2018_09_05_101721) do
     t.bigint "document_id"
     t.bigint "user_id"
     t.integer "order"
+    t.boolean "checked", default: false
     t.index ["document_id"], name: "index_sections_on_document_id"
     t.index ["user_id"], name: "index_sections_on_user_id"
   end
