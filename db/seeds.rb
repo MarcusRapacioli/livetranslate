ec14156e9e07c7653dee2dcd434e31f3622cd4cf
Marcus Rapacioli <marcus.rapacioli@gmail.com>
2018-09-06 19:51:39 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,14 @@ User.create!(first_name: "Warren", last_name: "Buffet", email: "warren@me.com", 
 
 georges = User.create!(first_name: "Georges", last_name: "Haines", email: "georges@me.com", password: "moolah", user_type: "Student", institution: "Durham Uni")
 ben = User.create!(first_name: "Ben", last_name: "Riley", email: "ben@me.com", password: "moolah", user_type: "Student", institution: "Durham Uni")
-katie = User.create!(first_name: "Katie", last_name: "Pitcher", email: "katie@me.com", password: "moolah", user_type: "Student", institution: "Durham Uni")
+yuichi = User.create!(first_name: "Yuichi", last_name: "Toyama", email: "yuichi@me.com", password: "moolah", user_type: "Student", institution: "Durham Uni")
 
 russian = Lesson.create!(lesson_name: "Russian - Beginners", teacher: User.first)
 italian = Lesson.create!(lesson_name: "Italian - Intermediate", teacher: User.first)
 japanese = Lesson.create!(lesson_name: "Japanese - Advanced", teacher: User.first)
 french = Lesson.create!(lesson_name: "French - Beginners", teacher: User.first)
 
-[georges, ben, katie].each do |student|
+[georges, ben, yuichi].each do |student|
   StudentLesson.create!(student: student, lesson: russian)
   StudentLesson.create!(student: student, lesson: italian)
   StudentLesson.create!(student: student, lesson: french)
